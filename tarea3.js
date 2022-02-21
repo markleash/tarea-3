58c49e75f135238af5753ae7c42240a93f523e16
markleash <74120689+markleash@users.noreply.github.com>
2022-02-21 10:40:19 +0100

    Arreglo de bucles for y la función calcular tiempo
    Las lineas 7, 13 y 19 devolvían un error de falta de value al tener el símbolo =

--- tarea3.js
+++ tarea3.js
@@ -1,19 +1,38 @@
-const segundosEnMinuto = 60;
-const minutosEnHora = 60;
 const botonCalcular = document.querySelector("#calcular")
 
 
-
-
 botonCalcular.onclick = function(){
-    const totalSegundos = Number(document.querySelectorAll(".segundos").values)
-    const totalMinutos = Number(document.querySelectorAll(".minutos").values)
-    const totalHoras = Number(document.querySelectorAll(".horas").values)
+    const segundosVideos = document.querySelectorAll(".segundos")
+    let totalSegundos = 0;
+    for (i=0; i<segundosVideos.length; i++){
+        totalSegundos += Number(segundosVideos[i].value)
+    }
 
-    let totalTiempo = Number(totalSegundos + totalMinutos + totalHoras);
+    const minutosVideos = document.querySelectorAll(".minutos")
+    let totalMinutos = 0;
+    for (j=0; j<minutosVideos.length; j++){
+        totalMinutos += Number(minutosVideos[j].value)
+    }
+    
+    const horasVideos = document.querySelectorAll(".horas")
+    let totalHoras = 0;
+    for (k=0; k<horasVideos.length; k++){
+        totalHoras += Number(horasVideos[k].value)
+    }
 
+
+   const segundosEnMinutos = Math.floor(totalSegundos/ 60);
+   const segundosRestantes = totalSegundos % 60;
+
+   const totalMinutosSumados = totalMinutos + segundosEnMinutos;
+   const minutosEnHoras = Math.floor(totalMinutosSumados / 60);
+   const minutosRestantes = totalMinutosSumados % 60;
+
+   const totalHorasSumadas = totalHoras + minutosEnHoras;
+
+    let totalTiempo = `El resultado de los videos es de ${totalHorasSumadas} horas, ${minutosRestantes} minutos y ${segundosRestantes} segundos`;
     document.querySelector(".resultado").innerText = totalTiempo;
+
     return false;
 }
 
-console.log(botonCalcular)
